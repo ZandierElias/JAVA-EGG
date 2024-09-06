aba7525db0ddf1ac0287157124722e2f48b250b9
Zandier Raymundo <161496102+ZandierElias@users.noreply.github.com>
2024-09-06 12:32:27 -0500

    Bucle I - activdad Elementos Orden Inverso

---
+++ 9_Bucles_I/src/OrdenInverso.java
@@ -0,0 +1,16 @@
+
+public class OrdenInverso {
+
+    public static void main(String[] args) {
+        //Escribe un programa que recorra un array de enteros y los imprima en orden inverso, comenzando desde el último elemento.. El tamaño y los números  a contener pueden ser de tu elección.
+
+        // creando array
+        int[] arreglo = {10, 20, 30, 50, 65, 43};
+        // creando bucle para imprimir elementos inversamente
+        for (int i = arreglo.length - 1; i >= 0; i--) {
+            System.out.println("Numero " + (i + 1) + " es : " + arreglo[i]);
+
+        }
+
+    }
+}
