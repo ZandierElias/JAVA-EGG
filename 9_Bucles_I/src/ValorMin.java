2ba76a791b69e785a4abee58112984a7265e564f
Zandier Raymundo <161496102+ZandierElias@users.noreply.github.com>
2024-09-06 12:13:40 -0500

    Bucle I - actividad Encontrando valor minimo

---
+++ 9_Bucles_I/src/ValorMin.java
@@ -0,0 +1,20 @@
+
+public class ValorMin {
+
+    public static void main(String[] args) {
+        //Escribe un programa que recorra un array de enteros y encuentre el valor mínimo presente en el array. El tamaño y los números  a contener pueden ser de tu elección.
+
+        //array 
+        int[] arreglo = {43, 25, 65, 12, 32, 44, 16, 22};
+        // variable para valor minimo 
+        int minimo = arreglo[0];
+        // encontrar el menor 
+        for (int i = 1; i < arreglo.length; i++) {
+            if (arreglo[i] < minimo) {
+                minimo = arreglo[i];
+            }
+        }
+        System.out.println("El valor minimo dentro del array es: " + minimo);
+
+    }
+}
